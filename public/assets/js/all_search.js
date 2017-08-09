8fb55238fb0a379840236d42e764705fd4f169ee
bcmiller713 <bcmiller713@gmail.com>
2017-08-09 15:04:08 -0700

    matched id names of html and js files

--- public/assets/js/all_search.js
+++ public/assets/js/all_search.js
@@ -1,23 +1,21 @@
-$("#submit-btn").on("click", function(event) {
-  event.preventDefault();
-
+$("#all-search").on("click", function() {
+  
   var ingredients;
   var recipe;
   var username;
 
-	if ($("#ingredient-field").val().trim() === "" || $("#recipe-field").val().trim() === "" || $("#username-field").val().trim() === "") {
+	if ($("#all-recipeName").val().trim() === "" || $("#all-ingredient").val().trim() === "" || $("#all-author").val().trim() === "") {
       alert("Please fill out all search fields before submitting your search.");
   }
   else {
-    var recipeArr = $("#recipe-field").val().trim().split(" ");
+    var recipeArr = $("#all-recipeName").val().trim().split(" ");
     recipe = recipeArr.join("+");
-    var ingredientsArr = $("#ingredient-field").val().trim().split(", ");
+    var ingredientsArr = $("#all-ingredient").val().trim().split(", ");
     for (var i = 0; i < ingredientsArr.length; i++) {
         ingredientsArr[i].replace(/\s/g, "+");
     }
     ingredients = ingredientsArr.toString();
-    username = $("#username-field").val().trim();
-
+    username = $("#all-author").val().trim();
   }
 
   window.location = "/api/search/" + recipe + "/" + ingredients + "/" + username;
--- public/assets/js/username_search.js
+++ public/assets/js/username_search.js
@@ -1,11 +1,10 @@
-$("#search-btn").on("click", function(event) {
-    event.preventDefault();
+$("#author-name-button").on("click", function() {
     var username;
-    if ($("#search-field").val().trim() === "") {
+    if ($("#author-name").val().trim() === "") {
         alert("Please complete the search field before submitting your search");
     }
     else {
-        username = $("#search-field").val().trim();
+        username = $("#author-name").val().trim();
     }
     window.location = "/api/search/null/null/" + username;
 });
